91fd7b97f16790aaa5d0e064bfa707200307cd5f
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-09-20 07:26:58 +0000

    ok

--- modules/open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/service/ContactTemplateServiceImpl.java
+++ modules/open-auction-template/src/main/java/com/axelor/apps/openauctiontemplate/service/ContactTemplateServiceImpl.java
@@ -28,7 +28,7 @@ public class ContactTemplateServiceImpl implements ContactTemplateService {
   @Override
   @Transactional
   public Partner createContactFromTemplate(ContactTemplate contactTemplate, Partner tmpPartner) {
-    log.debug("Creation d'un contact depuis un modèle de contact");
+    log.debug("Creation d'un contact depuis un modèle");
     tmpPartner.setIsContact(true);
     tmpPartner.setIsCustomer(true);
     Set<Company> set = new HashSet<Company>(companyRepository.all().fetch());
