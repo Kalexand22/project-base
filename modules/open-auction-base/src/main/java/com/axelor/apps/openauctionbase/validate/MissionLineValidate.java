658ac7ec6f079bd1873e8359152f4abe24225747
isatech-eq <eric.quester@isatech.fr>
2022-10-18 14:25:24 +0200

    ok

--- modules/open-auction-base/src/main/java/com/axelor/apps/openauctionbase/validate/MissionLineValidate.java
+++ modules/open-auction-base/src/main/java/com/axelor/apps/openauctionbase/validate/MissionLineValidate.java
@@ -14,6 +14,7 @@ import com.axelor.exception.db.repo.TraceBackRepository;
 import com.axelor.inject.Beans;
 import java.math.BigDecimal;
 
+
 public class MissionLineValidate {
   /*
    * OnValidate=BEGIN
