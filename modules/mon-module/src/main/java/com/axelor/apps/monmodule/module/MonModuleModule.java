9a506a0666e956b291bb608a342977a71f3eacfe
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-10-19 22:22:25 +0200

    ok

---
+++ modules/mon-module/src/main/java/com/axelor/apps/monmodule/module/MonModuleModule.java
@@ -0,0 +1,9 @@
+package com.axelor.apps.monmodule.module;
+
+import com.axelor.app.AxelorModule;
+
+public class MonModuleModule extends AxelorModule {
+
+  @Override
+  protected void configure() {}
+}
