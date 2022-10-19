f56ad1e5458f3f16f86c1f3171e55bdf05a313b8
Kalexand22 <karl.alexandersson@it-innovations-bzh.fr>
2022-10-19 22:22:34 +0200

    ok

--- modules/mon-module/src/main/java/com/axelor/apps/openauctionbase/module/OpenAuctionBaseModule.java
+++
@@ -1,9 +0,0 @@
-package com.axelor.apps.openauctionbase.module;
-
-import com.axelor.app.AxelorModule;
-
-public class OpenAuctionBaseModule extends AxelorModule {
-
-  @Override
-  protected void configure() {}
-}
