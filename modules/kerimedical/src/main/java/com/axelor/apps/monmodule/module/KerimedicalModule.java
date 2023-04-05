ef295dee9202d9ece4ed792d7210ce76cfd1602d
jlostanlen <jeremie.lostanlen@gmail.com>
2023-04-05 12:02:01 +0000

    ok

--- modules/kerimedical/src/main/java/com/axelor/apps/monmodule/module/KerimedicalModule.java
+++ modules/kerimedical/src/main/java/com/axelor/apps/monmodule/module/KerimedicalModule.java
@@ -2,7 +2,7 @@ package com.axelor.apps.monmodule.module;
 
 import com.axelor.app.AxelorModule;
 
-public class MonModuleModule extends AxelorModule {
+public class KerimedicalModule extends AxelorModule {
 
   @Override
   protected void configure() {}
